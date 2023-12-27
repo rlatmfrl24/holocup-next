219a853a25593587614905b4cc9874d78712c15a
SulfurBottom <397love@gmail.com>
2023-12-28 08:15:27 +0900

    round selecter 표시 로직 추가 webp 변경

--- src/app/records/page.tsx
+++ src/app/records/page.tsx
@@ -1,19 +1,28 @@
+"use client";
+
 import Overview from "./components/overview";
 import CupSelector from "./components/cup_selector";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import RoundSelector from "./components/round_selector";
+import { useState } from "react";
 
-export default async function Records() {
+export default function Records() {
+  const [tabValue, setTabValue] = useState("overview");
+
   return (
     <div className="p-4 flex-1 container mx-auto ">
       <h1 className="text-4xl font-extrabold mb-3">Records</h1>
       <Tabs defaultValue="overview">
         <TabsList className="mr-2">
-          <TabsTrigger value="overview">Overview</TabsTrigger>
-          <TabsTrigger value="rounds">Rounds</TabsTrigger>
+          <TabsTrigger value="overview" onClick={() => setTabValue("overview")}>
+            Overview
+          </TabsTrigger>
+          <TabsTrigger value="rounds" onClick={() => setTabValue("rounds")}>
+            Rounds
+          </TabsTrigger>
         </TabsList>
         <CupSelector />
-        <RoundSelector />
+        {tabValue === "rounds" && <RoundSelector />}
         <TabsContent value="overview">
           <Overview />
         </TabsContent>
